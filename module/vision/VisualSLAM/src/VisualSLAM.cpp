9b3f72f25eb49b637522d73015f5d258dc5c19b0
Daniel Ginn <daniel.ginn@live.com.au>
2018-02-02 17:14:10 +1100

    Image emitting function works properly now

--- module/vision/VisualSLAM/src/VisualSLAM.cpp
+++ module/vision/VisualSLAM/src/VisualSLAM.cpp
@@ -45,7 +45,6 @@ namespace vision {
         //});
 
         on<Startup>().then([this] {
-
             // Loading in timestamp and image filepath data
             LoadImages("/home/vagrant/Datasets/NUbotsRoom_Dataset1/", vstrImageFilenames, vTimestamps);
             nImages = vstrImageFilenames.size();
@@ -54,25 +53,15 @@ namespace vision {
 
         // Emitting loaded images
         // Loop two times per second
-        on<Every<2,std::chrono::seconds>>().then([this] {
+        on<Every<500,std::chrono::milliseconds>>().then([this] {
 
-            std::cout << "Starting Image Emitting Loop" << std::endl;
             if (curImage < nImages) {
                 loop_start_time = NUClear::clock::now();
                 loop_start_timestamp = vTimestamps.at(curImage);
                 std::chrono::milliseconds halfASecond(500);
 
-                // This is our first image
-                if (curImage == 0) {
-                    start_time = NUClear::clock::now();
-                    first_timecode = vTimestamps.at(0);
-                    std::cout << "First image setup. Start_time: " << start_time.time_since_epoch().count() << ", first_timecode: " << first_timecode.count() << std::endl;
-                }
-
                 while((vTimestamps.at(curImage)-loop_start_timestamp) < halfASecond) {
-                    std::cout << "Looping" << std::endl;
                     // Load image
-                    std::cout << "Loading image " << curImage << std::endl;
                     cv::Mat raw_image;
                     raw_image = cv::imread(vstrImageFilenames[curImage], CV_LOAD_IMAGE_UNCHANGED);
 
@@ -90,17 +79,21 @@ namespace vision {
                         }
                     }
 
-                    /* Create the image */
+                    // Create the image 
                     auto image = std::make_unique<Image>();
                     image->format     = utility::vision::FOURCC::GREY;
                     image->dimensions = {image_width, image_width};
                     image->data       = data;
 
+                    // This is our first image
+                    if (curImage == 0) {
+                        start_time = NUClear::clock::now();
+                        first_timecode = vTimestamps.at(0);
+                    }
+
                     // Work out when we should emit this packet
                     NUClear::clock::time_point emit_time = start_time + vTimestamps.at(curImage) - first_timecode;
-
                     emit<Scope::DELAY>(std::move(image),emit_time - NUClear::clock::now());
-
                     curImage++;
                     
                     // This is our last image
@@ -113,7 +106,6 @@ namespace vision {
         // Visual Odometry - motion estimation thread
         on<Trigger<Image>, Single>().then([this](const Image& newImage) {
 
-            std:: cout << "Image Received" << std::endl;
             // Sparse Model-based Image Alignment
             SparseImageAlignment sia;
             T_kkminus1 = sia.sparseImageAlignment(newImage);
@@ -121,8 +113,6 @@ namespace vision {
 
 
             // Pose and Structure Refinement
-
-
 
         });
 
