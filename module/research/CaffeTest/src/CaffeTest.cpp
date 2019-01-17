e7ba4f6d0220872901be8c7a814cd8d79b2a3f36
Daniel Ginn <daniel.ginn@live.com.au>
2019-01-17 16:08:17 +1100

    On Startup example added

--- module/research/CaffeTest/src/CaffeTest.cpp
+++ module/research/CaffeTest/src/CaffeTest.cpp
@@ -7,11 +7,14 @@ namespace research {
 
     using extension::Configuration;
 
-    CaffeTest::CaffeTest(std::unique_ptr<NUClear::Environment> environment)
-    : Reactor(std::move(environment)) {
+    CaffeTest::CaffeTest(std::unique_ptr<NUClear::Environment> environment) : Reactor(std::move(environment)) {
 
-        on<Configuration>("CaffeTest.yaml").then([this] (const Configuration& config) {
+        on<Configuration>("CaffeTest.yaml").then([this](const Configuration& config) {
             // Use configuration here from file CaffeTest.yaml
+        });
+
+        on<Startup, With<CameraParameters>>().then([this](const CameraParameters& cam) {
+            // On Startup example
         });
     }
 }
