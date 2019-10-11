78642e467ea287ce20d1d4efce06be6490ff1223
Daniel Ginn <daniel.ginn@live.com.au>
2019-10-11 18:28:32 +1100

    nbs updated to allow extract_images_MoCap

--- tools/nbs.py
+++ tools/nbs.py
@@ -3,6 +3,7 @@
 import nbs_tools.json
 import nbs_tools.stats
 import nbs_tools.extract_images
+import nbs_tools.extract_images_MoCap
 
 
 def register(command):
@@ -13,9 +14,10 @@ def register(command):
     nbs_tools.json.register(subcommands.add_parser("json"))
     nbs_tools.stats.register(subcommands.add_parser("stats"))
     nbs_tools.extract_images.register(subcommands.add_parser("extract_images"))
+    nbs_tools.extract_images.register(subcommands.add_parser("extract_images_MoCap"))
 
 
 def run(nbs_command, **kwargs):
-    {"stats": nbs_tools.stats.run, "json": nbs_tools.json.run, "extract_images": nbs_tools.extract_images.run}[
+    {"stats": nbs_tools.stats.run, "json": nbs_tools.json.run, "extract_images": nbs_tools.extract_images.run, "extract_images_MoCap": nbs_tools.extract_images_MoCap.run}[
         nbs_command
     ](**kwargs)
