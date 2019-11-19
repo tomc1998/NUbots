7ea1dd35942c5e09935b5763983b9edc5fd5dcf3
Ysobel Sims <ysobelsims@gmail.com>
2019-11-19 13:33:14 +1100

    Cleans foot controller code
    - *brushing noises*
    - and fix up config

--- module/motion/walk/MovementController/src/FootController.cpp
+++ module/motion/walk/MovementController/src/FootController.cpp
@@ -4,11 +4,13 @@ namespace module {
 namespace motion {
     namespace walk {
 
+        // Returns x-position of vector field. See vectorfield.py for graphical representation of the vector field
         double FootController::f_x(const Eigen::Vector3d& pos) {
             return (-pos.x() / std::abs(pos.x()))
                    * std::exp(-std::abs(std::pow(config.c * pos.x(), -config.step_steep)));
         }
 
+        // Returns z-position of vector field. See vectorfield.py for graphical representation of the vector field
         double FootController::f_z(const Eigen::Vector3d& pos) {
             return std::exp(-std::abs(std::pow(config.c * pos.x(), -config.step_steep)))
                    - (pos.z() / config.step_height);
@@ -17,9 +19,10 @@ namespace motion {
         /**
          * Calculates the next swing foot position we should be at in time_horizon amount of time
          *
+         * @param time_horizon Time into the future with which we project the next position to
          * @param time_left The amount of time remaining until we should hit our target
-         * @param Hf_wg homogeneous transform from ground space to current s"wing" (w) foot space
-         * @param Hf_wtg homogeneous transform from ground space to target s"wing" (w) foot space
+         * @param Hwg Homogeneous transform from ground space to current s"wing" (w) foot space
+         * @param Hng Homogeneous transform from ground space to next target s"wing" (w) foot space
          *
          * @return Hf_wng homogeneous transform from ground space to the horizon swing space (next swing space)
          */
@@ -35,23 +38,35 @@ namespace motion {
             //-------------VECTOR FIELD--------------------------------------
             //---------------------------------------------------------------
 
-            // Hw_gg is ground to ground-swing-target space. That is, the space the swing target is in, but with ground rotation.
+            // Hw_gg is ground to ground-swing-target space. That is, the space the swing target is in, but with ground
+            // rotation.
             Eigen::Affine3d Hw_gg = Hw_tg;
-            Hw_gg.linear() = Eigen::Matrix3d::Identity();
+            Hw_gg.linear()        = Eigen::Matrix3d::Identity();
 
+            // Get the wing target to wing vector
             Eigen::Affine3d Hw_gw    = Hw_gg * Hwg.inverse();
             Eigen::Vector3d rWW_gw_g = Hw_gw.translation();
+
+            // Use the vector field to get the wing target to next target position
             Eigen::Vector3d rNW_gw_g =
                 rWW_gw_g + (Eigen::Vector3d(f_x(rWW_gw_g), 0, f_z(rWW_gw_g)).normalized() * factor * rWW_gw_g.norm());
-            Eigen::Vector3d vector(Eigen::Vector3d(f_x(rWW_gw_g), 0, f_z(rWW_gw_g)).normalized() * factor
-                                   * rWW_gw_g.norm());
+
+            // Vector field does not take into account the y-value so we linearly interpolate so it will reach the
+            // target
             rNW_gw_g.y() = rWW_gw_g.y() * factor;
+
+            // Testing for the vector retrieved from the vector field
+            // Eigen::Vector3d vector(Eigen::Vector3d(f_x(rWW_gw_g), 0, f_z(rWW_gw_g)).normalized() * factor
+            //                       * rWW_gw_g.norm());
             // NUClear::log(vector.x(), ", ", vector.z());
+
+            // Take the vector into ground space
             Eigen::Vector3d rNGg = Hw_gg.inverse() * rNW_gw_g;
 
             //---------------------------------------------------------------
             //---------------------------------------------------------------
 
+            // Get the distance to the target
             double current_distance_target = rWW_gw_g.norm();
 
             // If we are very close to the target, just go to the target directly
@@ -59,6 +74,9 @@ namespace motion {
                 rNGg = Hw_gg.inverse().translation();
             }
 
+            // Create the Hgn matrix
+            // Slerp the rotation
+            // Set the translation
             Eigen::Affine3d Hgn;
             Hgn.linear() = Eigen::Quaterniond(Hwg.rotation())
                                .slerp(factor, Eigen::Quaterniond(Hw_tg.rotation()))
--- module/motion/walk/MovementController/src/FootController.h
+++ module/motion/walk/MovementController/src/FootController.h
@@ -11,11 +11,13 @@ namespace motion {
 
         class FootController {
         public:
+            // Calculates next swing position
             Eigen::Affine3d next_swing(const double& time_horizon,
                                        const double& time_left,
                                        const Eigen::Affine3d& Hwg,
                                        const Eigen::Affine3d& Hw_tg);
 
+            // Stores config variables for the vector field
             struct {
                 double step_height;
                 double well_width;
@@ -24,6 +26,8 @@ namespace motion {
             } config;
 
         private:
+            // Returns x and z positions of vector field. See vectorfield.py for graphical representation of vector
+            // field
             double f_x(const Eigen::Vector3d& pos);
             double f_z(const Eigen::Vector3d& pos);
 
