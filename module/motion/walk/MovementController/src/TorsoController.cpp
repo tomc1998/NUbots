fd86b08b68a69979ef9ca7ec2a2c0daeca5f366b
Ysobel <ysobelsims@gmail.com>
2020-01-22 12:31:16 +1100

    Change torso maths
    +commenting

--- module/motion/walk/MovementController/src/TorsoController.cpp
+++ module/motion/walk/MovementController/src/TorsoController.cpp
@@ -12,10 +12,10 @@ namespace motion {
          *
          * @param time_horizon Time into the future with which we project the next position to
          * @param time_left The amount of time remaining until we should hit our target
-         * @param Htg homogeneous transform from ground space to current torso space
-         * @param Ht_tg homogeneous transform from ground space to our target torso space
+         * @param Htg Homogeneous transform from ground space (g) to current torso space (t)
+         * @param Ht_tg Homogeneous transform from ground space (g) to our target torso space (t_t)
          *
-         * @return Hng homogeneous transform from ground space to the horizon torso space (next torso space)
+         * @return Hng Homogeneous transform from ground space (g) to the horizon torso space (next torso space (n))
          */
         Eigen::Affine3d TorsoController::next_torso(const double& time_horizon,
                                                     const double& time_left,
@@ -24,17 +24,34 @@ namespace motion {
             // Interpolate towards target
             double factor = time_horizon / time_left;
 
+            // The final homogeneous transform which we will build over the course of this function
             Eigen::Affine3d Hgn;
 
             // Interpolate the translation between current torso position and target position
-            Hgn.translation() = Htg.inverse().translation() * (1.0 - factor) + Ht_tg.inverse().translation() * factor;
+
+            // Get ground to torso in ground space and ground to torso target in ground space
+            // Htg.rotation().transpose() = Rgt
+            // Htg.translation() = rGTt
+            // -rGTt = rTGt
+            // Rgt * rTGt = rTGg
+            Eigen::Vector3d rTGg   = -Htg.rotation().transpose() * Htg.translation();
+            Eigen::Vector3d rT_tGg = -Ht_tg.rotation().transpose() * Ht_tg.translation();
+
+            // Get torso to torso target in ground space
+            // and then multiply it by factor to find the next position.
+            Eigen::Vector3d rT_tTg = rT_tGg - rTGg;
+            Eigen::Vector3d rNTg   = rT_tTg * factor;
+
+            // Set the translation of the final homogeneous transform
+            // rNGg = rNTg + rTGg
+            Hgn.translation() = rNTg + rTGg;
 
             // Slerp the rotation of the torso
-            Hgn.linear() = Eigen::Quaterniond(Htg.rotation())
-                               .slerp(factor, Eigen::Quaterniond(Ht_tg.rotation()))
+            Hgn.linear() = Eigen::Quaterniond(Ht_tg.rotation())
+                               .slerp(factor, Eigen::Quaterniond(Htg.rotation()))
                                .toRotationMatrix()
                                .transpose();
-            // Hgn.linear() = Htg.rotation().transpose();
+
             return Hgn.inverse();
         }
 
